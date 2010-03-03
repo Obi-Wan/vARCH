9b34fd19646f6334357367618bbf1567b3aa7677
Nicola <ben.vighy@gmail.com>
2010-03-03 17:11:21 +0100

    Start of chipset software interaction

---
+++ src/dummyChipset.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   dummyChipset.cpp
+ * Author: ben
+ * 
+ * Created on 3 marzo 2010, 14.34
+ */
+
+#include "dummyChipset.h"
+
+dummyChipset::dummyChipset() { }
+//
+//dummyChipset::dummyChipset(const dummyChipset& orig) { }
+//
+//dummyChipset::~dummyChipset() { }
+
+void
+dummyChipset::put(const short int& request, const int& arg) {
+  switch (request) {
+    case CHIP_INFO_SWORDS:
+      /* How many Single WORDS chip information takes */
+      break;
+    case CHIP_INFO_IN_MEM:
+      /* Let's put chip information in the memory area specified by arg */
+      break;
+    case DEVS_NUM:
+    case DEVS_LIST_IN_MEM:
+      break;
+    default:
+      break;
+  }
+}
+
---
+++ src/dummyChipset.h
@@ -0,0 +1,32 @@
+/* 
+ * File:   dummyChipset.h
+ * Author: ben
+ *
+ * Created on 3 marzo 2010, 14.34
+ */
+
+#ifndef _DUMMYCHIPSET_H
+#define	_DUMMYCHIPSET_H
+
+#include "Component.h"
+
+class dummyChipset : public Component {
+public:
+  dummyChipset();
+//  dummyChipset(const dummyChipset& orig);
+//  virtual ~dummyChipset();
+  
+  void put(const short int& request, const int& arg);
+
+  enum ChipsetActions {
+    CHIP_INFO_SWORDS  = 0x00,
+    CHIP_INFO_IN_MEM  = 0x01,
+    DEVS_NUM          = 0x02,
+    DEVS_LIST_IN_MEM  = 0x04,
+  };
+private:
+
+};
+
+#endif	/* _DUMMYCHIPSET_H */
+
