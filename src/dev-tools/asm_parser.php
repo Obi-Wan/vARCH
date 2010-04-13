d3d49ba8a6d29fbc096850c16f318f136037920a
Nicola <ben.vighy@gmail.com>
2010-04-14 00:27:24 +0200

    Lexer-Generator: modified to generate a bit more useful lexer.
    This lexer could really be a first lexical stage of an assembler.

--- src/dev-tools/asm_parser.php
+++ src/dev-tools/asm_parser.php
@@ -9,7 +9,12 @@ print <<<EOT
 
 %{
 #include <stdio.h>
+
+#define MAX_STR_CONST 256
 %}
+
+%s STRING
+%s COMMENT
 
 DIGIT    [0-9]
 LETTER   [a-zA-Z]
@@ -18,9 +23,9 @@ LETT_DIG ({LETTER}|{DIGIT})
 ID       {LETTER}{LETT_DIG}*
 KEYBKEYS ([ \\t]|{LETT_DIG}|[-!#$%&'()*+,./:;<=>?@[\\\\\\]^_`{|}~])
 
-LABEL    \\.{ID}+\\:
-AT_LABEL \\@{ID}+
-TO_LABEL \\.{ID}+
+LABEL    \\.{ID}\\:
+AT_LABEL \\@{ID}
+TO_LABEL \\.{ID}
 
 REGISTER \\%([RA][1-8]|[U]?SP)
 ADDR_REG \\(([RA][1-8]|[U]?SP)\\)
@@ -30,10 +35,12 @@ FLOAT    \\\$({DIGIT}+\\.{DIGIT}*|.{DIGIT}+)
 STRING   \\"{KEYBKEYS}+\\"
 
 %%
+             char string_buf[MAX_STR_CONST];
+             char *string_buf_ptr;
 
-{INTEGER}   fprintf( yyout, "INTEGER:     %s (%d)\\n", yytext, atoi(yytext+1) );
-{FLOAT}     fprintf( yyout, "FLOAT:       %s (%g)\\n", yytext, atof(yytext+1) );
-{STRING}    fprintf( yyout, "STRING:      %s\\n", yytext );
+<INITIAL>{
+ {INTEGER}   fprintf( yyout, "INTEGER(%d) ", atoi(yytext+1) );
+ {FLOAT}     fprintf( yyout, "FLOAT(%g) ", atof(yytext+1) );
 
 EOT;
 $output = "\n";
@@ -62,36 +69,73 @@ foreach ($cpuInstrs->childNodes as $instr) {
     unset ($instrCode);
   }
 }
-$output = substr($output,0, strlen($output)-1 ) . " {";
+$output = substr($output,0, strlen($output)-1 ) . " {\n";
 print $output;
 
 print <<<EOT
-            fprintf( yyout, "INSTRUCTION: %s\\n", yytext );
-          }
+             fprintf( yyout, "INSTRUCTION(%s) ", yytext );
+           }
      
-\.int|\.long|\.char|\.string|\.local|\.global|\.function|\.end {
-            fprintf( yyout, "KEYWORD:     %s\\n", yytext );
-          }
+ \.int|\.string|\.local|\.global {
+             fprintf( yyout, "KEYWORD(%s) ", yytext );
+           }
      
-{LABEL}     fprintf( yyout, "DEF_LABEL:   %s\\n", yytext );
-{TO_LABEL}  fprintf( yyout, "REF_LABEL:   %s\\n", yytext );
-{AT_LABEL}  fprintf( yyout, "ADDR_LABEL:  %s\\n", yytext );
-     
-{REGISTER}  fprintf( yyout, "REG:         %s\\n", yytext );
-{ADDR_REG}  fprintf( yyout, "ADDR_REG:    %s\\n", yytext );
-     
-{ID}        fprintf( yyout, "ID:          %s\\n", yytext );
+ {LABEL}     fprintf( yyout, "DEF_LABEL(%s) ", yytext );
+ {TO_LABEL}  fprintf( yyout, "REF_LABEL(%s) ", yytext );
+ {AT_LABEL}  fprintf( yyout, "ADDR_LABEL(%s) ", yytext );
+
+ {REGISTER}  fprintf( yyout, "REG(%s) ", yytext );
+ {ADDR_REG}  fprintf( yyout, "ADDR_REG(%s) ", yytext );
+
+ {ID}        fprintf( yyout, "ID(%s) ", yytext );
  
-"+"|"-"|"*"|"/" {
-            fprintf( yyout, "OP:          %s\\n", yytext );
-	  }
-     
-"{"[\^{}}\\n]*"}"  /* eat up one-line comments */
+ "+"|"-"|"*"|"/" {
+             fprintf( yyout, "OP(%s) ", yytext );
+           }
 
-^[ \\t]*";".*\\n	  /* one line comments*/
-     
-[ \\t\\n]+          /* eat up whitespace */
+ "/*"        BEGIN(COMMENT);
 
+ ";".*\\n     fprintf( yyout, "\\n" );
+
+ \" {
+             string_buf_ptr = string_buf;
+             BEGIN(STRING);
+	   }
+
+ \\n          fprintf( yyout, "\\n" );
+
+ {DIGIT}+{ID} {
+	     fprintf( yyout, "Error, unknown symbol: %s\\n", yytext );
+	     exit(1);
+	   }
+ [ \\t]+          /* eat up whitespace */
+
+}
+
+<COMMENT>{
+ \\n          fprintf( yyout, "\\n" );
+ "*/"        BEGIN(INITIAL);
+ .           /* eat up */
+}
+
+<STRING>{
+ \\\\(.|\\n) {
+             *string_buf_ptr++ = yytext[0];
+             *string_buf_ptr++ = yytext[1];
+	   }
+ 
+ [^\\\\\\n\\"]+ {
+             char *yptr = yytext;
+             while ( *yptr )
+               *string_buf_ptr++ = *yptr++;
+           }
+
+ \\" {
+             BEGIN(INITIAL);
+             *string_buf_ptr = '\\0';
+	     fprintf( yyout, "STRING(%s) ", string_buf );
+	   }
+}
 
 %%
 
@@ -109,6 +153,7 @@ main( int argc, char **argv ) {
     yyout = stdout;
   }
   yylex();
+  fprintf( yyout, "\\n" );
   return 0;
 }
 
