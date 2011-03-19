4539f27ae07918463071c4be4d49d2c418c8ef80
Nicola <ben.vighy@gmail.com>
2011-03-19 18:58:41 +0100

    Macros: reformatted and added useful macros
    Signed-off-by: Nicola <ben.vighy@gmail.com>

--- include/macros.h
+++ include/macros.h
@@ -49,24 +49,38 @@
 # include <iostream>
 using namespace std;
 
+# ifndef __PRETTY_FUNCTION__
+#   define __PRETTY_FUNCTION__ __FUNCTION__
+# endif
 #endif
 
+/* ********* Macros Definitions for Programmer's Use ***************************
+ * If the condition is false, it throws the exception
+ */
+#define CHECK_THROW( condition, exception ) do {\
+  if (!(condition)) {\
+    throw exception;\
+  }\
+} while (0)
+
 #ifdef DEBUG
-  #define DebugPrintf( x ) do { printf("Debug: "); printf x; } while(0)
-  #define DebugPrintfMaps( mapType, mapObj, mapName ) do { \
+# define INLINE
+# define DebugPrintf( x ) do { printf("Debug: "); printf x; } while(0)
+# define DebugReportException( x ) printf("Debug: %s", x.what())
+# define DebugPrintfMaps( mapType, mapObj, mapName ) do { \
     cout << "DEBUG: " << mapName << "\n"; \
     for(mapType::const_iterator i = mapObj.begin(); i != mapObj.end(); i++) { \
       cout << "first: \"" << i->first << "\", second: " << i->second << "\n"; \
     }\
   } while(0)
-  #define DebugPrintfLabels( mapObj, mapName ) do { \
+# define DebugPrintfLabels( mapObj, mapName ) do { \
     cout << "DEBUG: " << mapName << "\n"; \
     for(Labels::const_iterator i = mapObj.begin(); i != mapObj.end(); i++) { \
       cout << "name: \"" << i->first << "\", line: " << i->second.lineNumber \
            << " bytePos: " << i->second.byte << "\n"; \
     }\
   } while(0)
-  #define DebugPrintfCodeLines( obj, name ) do { \
+# define DebugPrintfCodeLines( obj, name ) do { \
     cout << "DEBUG: " << name << "\n"; \
     for(CodeLines::const_iterator i = obj.begin(); i != obj.end(); i++) { \
       cout << "Line: " << i->lineNumber << ", segments: "; \
@@ -78,22 +92,28 @@ using namespace std;
     }\
   } while(0)
 #else
-  #define DebugPrintf( x )
-  #define DebugPrintfLabels( mapObj, mapName )
-  #define DebugPrintfMaps( mapType, mapObj, mapName )
-  #define DebugPrintfCodeLines( obj, name )
+# define INLINE inline
+# define DebugPrintf( x )
+# define DebugReportException( x )
+# define DebugPrintfLabels( mapObj, mapName )
+# define DebugPrintfMaps( mapType, mapObj, mapName )
+# define DebugPrintfCodeLines( obj, name )
 #endif
 
 #ifdef WARNING
-  #define WarningPrintf( x ) do { printf("Warning: "); printf x; } while(0)
+# define WarningPrintf( x ) do { printf("Warning: "); printf x; } while(0)
+# define WarningReportException( x ) printf("Warning: %s", x.what())
 #else
-  #define WarningPrintf( x )
+# define WarningPrintf( x )
+# define WarningReportException( x )
 #endif
 
 #ifdef INFO
-  #define InfoPrintf( x ) do { printf("Info: "); printf x; } while(0)
+# define InfoPrintf( x ) do { printf("Info: "); printf x; } while(0)
+# define InfoReportException( x ) printf("Info: %s", x.what())
 #else
-  #define InfoPrintf( x )
+# define InfoPrintf( x )
+# define InfoReportException( x )
 #endif
 
 #endif	/* _MACROS_H */
