643090354cdfe976756f39fd937ba94182609dd0
Nicola <ben.vighy@gmail.com>
2011-07-27 23:16:24 +0200

    vAsm: added check on writable destination of instructions
    Signed-off-by: Nicola <ben.vighy@gmail.com>

--- src/vasm/asm-classes.cpp
+++ src/vasm/asm-classes.cpp
@@ -13,7 +13,9 @@
 using namespace std;
 
 void
-asm_instruction_statement::checkArgs() const {
+asm_instruction_statement::checkArgs() const
+{
+  // Check size
   size_t instrNumOfArgs = (instruction >> 30 ) & 3;
   if (instrNumOfArgs != args.size()) {
     stringstream stream;
@@ -25,5 +27,48 @@ asm_instruction_statement::checkArgs() const {
             << " - Arguments expected: " << instrNumOfArgs
               << ", passed: " << args.size() << endl;
     throw WrongArgumentException(stream.str());
+  }
+
+  // Check writable destination
+  switch (instruction) {
+    case NOT:
+    case INCR:
+    case DECR:
+    case COMP2:
+    case LSH:
+    case RSH: {
+      if (args[0]->type == COST) {
+        stringstream stream;
+        stream  << "Expected non constant argument for unary instruction '"
+                  << ISet.getIstr(instruction)
+                  << "' (" << instruction << ") at position:" << endl
+                << position.fileNode->printString()
+                  << " Line: " << position.first_line << endl;
+        throw WrongArgumentException(stream.str());
+      }
+      break;
+    }
+    case ADD:
+    case MULT:
+    case SUB:
+    case DIV:
+    case QUOT:
+    case AND:
+    case OR:
+    case XOR:
+    case GET: {
+      if (args[1]->type == COST) {
+        stringstream stream;
+        stream  << "Expected non constant destination argument for binary "
+                  << "instruction '" << ISet.getIstr(instruction)
+                  << "' (" << instruction << ") at position:" << endl
+                << position.fileNode->printString()
+                  << " Line: " << position.first_line << endl;
+        throw WrongArgumentException(stream.str());
+      }
+      break;
+    }
+    default:
+      break;
   }
 }
