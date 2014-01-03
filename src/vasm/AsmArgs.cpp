e7280b6335e83ff87bc9f17bde43d68843066331
Nicola Viganò <ben.vighy@gmail.com>
2014-01-03 01:19:27 +0100

    vAsm(/vIR): renamed a few files to conform with future documentation
    Signed-off-by: Nicola Viganò <ben.vighy@gmail.com>

--- src/vasm/AsmArgs.cpp
+++ src/vasm/AsmArgs.cpp
@@ -15,25 +15,30 @@ using namespace std;
 void
 AsmArgs::parse()
 {
-  for(int32_t argNum = 1; argNum < argc; argNum++) {
+  for(int32_t argNum = 1; argNum < argc; argNum++)
+  {
     const string tempArg(argv[argNum]);
 
-    if (!tempArg.compare("-c")) {
+    if (!tempArg.compare("-c"))
+    {
       onlyCompile = true;
-
-    } else if (!tempArg.compare("-o")) {
+    }
+    else if (!tempArg.compare("-o"))
+    {
       /* Tells which file will be the output */
       CHECK_THROW( outputName.empty(),
           WrongArgumentException("Double definition of output file") );
       CHECK_THROW( (++argNum < argc),
           WrongArgumentException("You didn't specify the output name") );
       outputName.assign(argv[argNum]);
-
-    } else if (!tempArg.substr(0, 2).compare("-I")) {
+    }
+    else if (!tempArg.substr(0, 2).compare("-I"))
+    {
       /* Tells to append an include dir */
       includeDirs.push_back(tempArg.substr(2, tempArg.size()-2));
-
-    } else if (!tempArg.substr(0, 2).compare("-O")) {
+    }
+    else if (!tempArg.substr(0, 2).compare("-O"))
+    {
       /* Tells to append an include dir */
       CHECK_THROW( tempArg.size() == 3,
           WrongArgumentException(
@@ -44,14 +49,17 @@ AsmArgs::parse()
       CHECK_THROW( optimLevel >= 0 && optimLevel <= 3,
           WrongArgumentException(
               "Optimization Level should be between 0 and 3") );
-
-    } else if (!tempArg.compare("-reg-auto-alloc")) {
+    }
+    else if (!tempArg.compare("-reg-auto-alloc"))
+    {
       regAutoAlloc = true;
-
-    } else if (!tempArg.compare("-reg-coalesce")) {
+    }
+    else if (!tempArg.compare("-reg-coalesce"))
+    {
       regCoalesce = true;
-
-    } else if (!tempArg.compare("-d")) {
+    }
+    else if (!tempArg.compare("-d"))
+    {
       /* Tells which file will be the output */
       CHECK_THROW( debugSymbolsName.empty(),
           WrongArgumentException("Double definition of Debug Symbols file") );
@@ -59,39 +67,49 @@ AsmArgs::parse()
           WrongArgumentException(
               "You didn't specify the Debug Symbols file name") );
       debugSymbolsName.assign(argv[argNum]);
-
-    } else if (!tempArg.compare("-only-validate")) {
+    }
+    else if (!tempArg.compare("-only-validate"))
+    {
       onlyValidate = true;
-
-    } else if (!tempArg.compare("-disassemble-result")) {
+    }
+    else if (!tempArg.compare("-disassemble-result"))
+    {
       disassembleResult = true;
 
-    } else if (!tempArg.compare("-fomit-frame-pointer")) {
+    }
+    else if (!tempArg.compare("-fomit-frame-pointer"))
+    {
       omitFramePointer = true;
-
-    } else if (!tempArg.compare("-h")) {
+    }
+    else if (!tempArg.compare("-h"))
+    {
       throw WrongArgumentException("");
-    } else {
+    }
+    else
+    {
       inputFiles.push_back(argv[argNum]);
     }
   }
 
   InfoPrintf(("Input filenames (Source): \n"));
 #ifdef INFO
-  for (const string & filename : this->getSrcInputNames()) {
+  for (const string & filename : this->getSrcInputNames())
+  {
     InfoPrintf(("  - %s\n", filename.c_str()));
   }
 #endif
   InfoPrintf(("Input filenames (Object): \n"));
 #ifdef INFO
-  for (const string & filename : this->getObjInputNames()) {
+  for (const string & filename : this->getObjInputNames())
+  {
     InfoPrintf(("  - %s\n", filename.c_str()));
   }
 #endif
   InfoPrintf(("Output filename: %s\n", outputName.c_str()));
 #ifdef INFO
   string includes;
-  for(uint32_t inclNum = 0; inclNum < includeDirs.size(); inclNum++) {
+  for(uint32_t inclNum = 0; inclNum < includeDirs.size(); inclNum++)
+  {
     includes += " " + includeDirs[inclNum];
   }
 #endif
@@ -120,7 +138,7 @@ AsmArgs::parse()
 void
 AsmArgs::printHelp() const throw()
 {
-  cout << "Syntax is: 'new-asm <input_files> -o <output_file> [options]'" << endl;
+  cout << "Syntax is: 'vasm <input_files> -o <output_file> [options]'" << endl;
   cout << " -only-validate : only parse, without emitting code" << endl;
   cout << " -c : Just compiles, and output a static object file" << endl;
   cout << " -I<include_path> : adds an include path" << endl;
